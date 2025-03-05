8f18367dcf8427d12a1200bfe9dd14b733116bb9
MrSmoke <709976+MrSmoke@users.noreply.github.com>
2025-03-05 15:07:59 +1100

    Add more EnumrableExtesion tests

--- src/Primitives/Primitives/test/Extensions/EnumerableExtensionTests.cs
+++ src/Primitives/Primitives/test/Extensions/EnumerableExtensionTests.cs
@@ -6,11 +6,38 @@ using Xunit;
 public class EnumerableExtensionTests
 {
     [Fact]
-    public void TryGetFirstValue()
+    public void TryGetFirstValue_Array()
     {
         int[] list = [1, 2, 3];
 
         Assert.True(list.TryGetFirstValue(out var value));
         Assert.Equal(1, value);
+    }
+
+    [Fact]
+    public void TryGetFirstValue_Enumerable()
+    {
+        // Where() to force an Enumerable
+        IEnumerable<int> list = Enumerable.Range(1, 100).Where(x => true);
+
+        Assert.True(list.TryGetFirstValue(out var value));
+        Assert.Equal(1, value);
+    }
+
+    [Fact]
+    public void TryGetFirstValue_Empty()
+    {
+        int[] list = [];
+
+        Assert.False(list.TryGetFirstValue(out _));
+    }
+
+    [Fact]
+    public void TryGetFirstValue_EmptyEnumerable()
+    {
+        // Where() to force an Enumerable
+        IEnumerable<int> list = Enumerable.Range(1, 100).Where(x => false);
+
+        Assert.False(list.TryGetFirstValue(out _));
     }
 }
